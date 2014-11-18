9b3d870016c704cd37859fe517d64e7bfe9b9dbf
Adam Hueniken <adam.hueniken@gmail.com>
2014-11-18 09:51:41 -0500

    Added images for apps

--- app/assets/javascripts/actions.js
+++ app/assets/javascripts/actions.js
@@ -1,9 +1,9 @@
 
-
+/*
 $(document).ready(
          function() {
           setInterval(function() {
             $('.dynamic').load('/actions/refresh');
         }, 3000);
         console.log("test");
-    });
+    }); */
--- app/helpers/actions_helper.rb
+++ app/helpers/actions_helper.rb
@@ -1,2 +1,22 @@
 module ActionsHelper
+
+	def getAppIcon(appName) 
+		case appName
+		when 'chrome'
+			'chrome.jpg'
+		when 'calendar'
+			'calendar.png'
+		when 'maps'
+			'googlemaps.png'
+		when 'youtube'
+			'youtube.png'
+		when 'twitter'
+			'twitter.png'
+		when 'launcher'
+			'ic_launcher.png'
+		else
+			'nonefound.png'
+		end
+	end
+
 end
