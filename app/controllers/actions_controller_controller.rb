a1562d48fc345faff7ade83e58e53d1ab25d7cfa
Adam Hueniken <adam.hueniken@gmail.com>
2014-11-18 07:05:34 -0500

    Removed destroyed files, updated asset_pipeline for heroku

--- app/controllers/actions_controller_controller.rb
+++
@@ -1,7 +0,0 @@
-class ActionsControllerController < ApplicationController
-  def show
-  end
-
-  def share
-  end
-end
--- test/helpers/actions_controller_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class ActionsControllerHelperTest < ActionView::TestCase
-end
