b5b9beaa9fd36d91091fb075f8e825c1788dc162
Adam Hueniken <adam.hueniken@gmail.com>
2014-11-18 06:21:01 -0500

    Added image for users, improved list of actions display

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141118084122) do
+ActiveRecord::Schema.define(version: 20141118105049) do
 
   create_table "actions", force: true do |t|
     t.datetime "created_at"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20141118084122) do
   create_table "users", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_url"
   end
 
 end
