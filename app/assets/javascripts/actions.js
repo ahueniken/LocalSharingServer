1e64a3621cf0af78c4bcd03e212cd7cd3ebf380c
Adam Hueniken <adam.hueniken@gmail.com>
2015-02-10 09:13:19 -0500

    Updated ajax call to take params to refresh call

--- app/assets/javascripts/actions.js
+++ app/assets/javascripts/actions.js
@@ -1,7 +1,10 @@
 $(document).ready(
          function() {
+         	var pathname = window.location.pathname;
+         	var search = window.location.search;
+         	console.log(pathname + search);
           setInterval(function() {
-            $('.dynamic').load('/actions/refresh');
+            $('.dynamic').load("/actions/refresh/" + search);
         }, 3000);
         console.log("test");
     }); 
